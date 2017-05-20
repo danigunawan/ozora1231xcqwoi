81dfead86cb560842e0cd5f0c838c757960cdc6c
Ghazamalghifari <ghozam.alghifari96@gmail.com>
2017-05-20 14:12:08 +0700

    memperbaiki master toko

--- tabel-toko.php
+++ tabel-toko.php
@@ -1,198 +1,92 @@
-<?php session_start();
-
-
+<?php include 'session_login.php';
+/* Database connection start */
 include 'sanitasi.php';
 include 'db.php';
-$session_id = session_id();
 
-$query = $db->query("SELECT * FROM toko");
+/* Database connection end */
 
 
+// storing  request (ie, get/post) global array to a variable  
+$requestData= $_REQUEST;
+
+$columns = array( 
+// datatable column index  => database column name
+
+    
+    0=>'Nama toko', 
+    1=>'Alamat toko', 
+    2=>'Hapus',
+    3=>'Edit',
+    4=>'id' 
+
+);
+
+// getting total number records without any search
+$sql =" SELECT id,nama_toko,alamat_toko ";
+$sql.=" FROM toko ";
+$sql.="";
+
+$query = mysqli_query($conn, $sql) or die("eror 1");
+$totalData = mysqli_num_rows($query);
+$totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
+
+if( !empty($requestData['search']['value']) ) {   // if there is a search parameter, $requestData['search']['value'] contains search parameter
+$sql =" SELECT id,nama_toko,alamat_toko ";
+$sql.=" FROM toko ";
+$sql.=" WHERE 1=1 ";
+
+    $sql.=" AND (nama_toko LIKE '".$requestData['search']['value']."%' )";   
+    $sql.=" AND (alamat_toko LIKE '".$requestData['search']['value']."%' )";   
+}
+
+
+$query=mysqli_query($conn, $sql) or die("eror 2");
+$totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
+        
+$sql.=" ORDER BY id ".$requestData['order'][0]['dir']."  LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
+
+/* $requestData['order'][0]['column'] contains colmun index, $requestData['order'][0]['dir'] contains order such as asc/desc  */    
+$query=mysqli_query($conn, $sql) or die("eror 3");
+
+
+$data = array();
+while( $row=mysqli_fetch_array($query) ) {  // preparing an array
+  $nestedData=array();
+
+         $query_otoritas_master_data_toko = $db->query("SELECT toko_edit,toko_hapus FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' ");
+		$data_otoritas_master_data_toko = mysqli_fetch_array($query_otoritas_master_data_toko);
+
+
+          $nestedData[] = $row['nama_toko'];
+          $nestedData[] = $row['alamat_toko'];
+          if ($data_otoritas_master_data_toko['toko_edit'] == 1){
+          $nestedData[] = "<button class='btn btn-danger btn-hapus btn-sm' data-id='". $row['id'] ."' data-toko='". $row['nama_toko'] ."'> <span class='glyphicon glyphicon-trash'> </span> Hapus </button>";
+          }
+          else{
+			$nestedData[] = "<p></p>";
+          }
+
+          if ($data_otoritas_master_data_toko['toko_hapus'] == 1){
+          $nestedData[] = "<button class='btn btn-success btn-edit btn-sm' data-toko='". $row['nama_toko'] ."' data-alamat='". $row['alamat_toko'] ."' data-id='". $row['id'] ."' > <span class='glyphicon glyphicon-edit'> </span> Edit </button>";
+      	  }
+      	  else{
+			$nestedData[] = "<p></p>";
+      	  }
+
+          $nestedData[] = $row['id'];
+          
+  $data[] = $nestedData;
+}
+
+
+
+$json_data = array(
+            "draw"            => intval( $requestData['draw'] ),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
+            "recordsTotal"    => intval( $totalData ),  // total number of records
+            "recordsFiltered" => intval( $totalFiltered ), // total number of records after searching, if there is no searching then totalFiltered = totalData
+            "data"            => $data   // total data array
+            );
+
+echo json_encode($json_data);  // send data as json format
 
  ?>
-
-
-<table id="tableuser" class="table table-bordered">
-		<thead> 
-			
-			<th> Nama toko </th>
-			<th> Alamat Toko</th>
-<?php  
-include 'db.php';
-
-$pilih_akses_toko_hapus = $db->query("SELECT toko_hapus FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_hapus = '1'");
-$toko_hapus = mysqli_num_rows($pilih_akses_toko_hapus);
-
-
-    if ($toko_hapus > 0){
-			echo "<th> Hapus </th>";
-		}
-?>
-
-<?php 
-include 'db.php';
-
-$pilih_akses_toko_edit = $db->query("SELECT toko_edit FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_edit = '1'");
-$toko_edit = mysqli_num_rows($pilih_akses_toko_edit);
-
-
-    if ($toko_edit > 0){
-    	echo "<th> Edit </th>";
-    }
- ?>
-			
-			
-		</thead>
-		
-		<tbody>
-		<?php
-
-		// menyimpan data sementara yang ada pada $query
-			while ($data = mysqli_fetch_array($query))
-			{
-				//menampilkan data
-			echo "<tr>
-			
-			<td>". $data['nama_toko'] ."</td>
-			<td>". $data['alamat_toko'] ."</td>";
-
-include 'db.php';
-
-$pilih_akses_toko_hapus = $db->query("SELECT toko_hapus FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_hapus = '1'");
-$toko_hapus = mysqli_num_rows($pilih_akses_toko_hapus);
-
-
-    if ($toko_hapus > 0){
-
-			echo "<td> <button class='btn btn-danger btn-hapus' data-id='". $data['id'] ."' data-toko='". $data['nama_toko'] ."'> <span class='glyphicon glyphicon-trash'> </span> Hapus </button> </td>";
-		}
-
-include 'db.php';
-
-$pilih_akses_toko_edit = $db->query("SELECT toko_edit FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_edit = '1'");
-$toko_edit = mysqli_num_rows($pilih_akses_toko_edit);
-
-
-    if ($toko_edit > 0){ 
-			echo "<td> <button class='btn btn-info btn-edit'  data-alamat='". $data['alamat_toko'] ."' data-id='". $data['id'] ."' data-toko='". $data['nama_toko'] ."' > <span class='glyphicon glyphicon-edit'> </span> Edit </button> </td>
-			</tr>";
-			}
-	}
-
-	//Untuk Memutuskan Koneksi Ke Database
-mysqli_close($db);   
-		?>
-		</tbody>
-
-	</table>
-
-<script>
-    $(document).ready(function(){
-	
-//fungsi hapus data 
-		$(".btn-hapus").click(function(){
-		var nama = $(this).attr("data-toko");
-		var id = $(this).attr("data-id");
-		$("#data_toko").val(nama);
-		$("#id_hapus").val(id);
-		$("#modal_hapus").modal('show');
-		
-		
-		});
-
-
-		$("#btn_jadi_hapus").click(function(){
-		
-		var id = $("#id_hapus").val();
-
-		$.post("proses_hapus_toko.php",{id:id},function(data){
-
-		if (data != "") {
-		$("#table_baru").load('tabel-toko.php');
-		$("#modal_hapus").modal('hide');
-		
-		}
-
-		
-		});
-		
-		});
-// end fungsi hapus data
-
-//fungsi edit data 
-		$(".btn-edit").click(function(){
-		
-		$("#modal_edit").modal('show');
-		var nama_toko = $(this).attr("data-toko"); 
-		var alamat_toko = $(this).attr("data-alamat");
-		var id  = $(this).attr("data-id");
-		$("#toko_edit").val(nama_toko);
-		$("#alamat_edit").val(alamat_toko);
-		$("#id_edit").val(id);
-		 
-		});
-		
-		$("#submit_edit").click(function(){
-		var nama_toko = $("#toko_edit").val();
-		var alamat_toko = $("#alamat_edit").val();
-		var id = $("#id_edit").val();
-
-		if (nama_toko == ""){
-			alert("Nama Harus Diisi");
-		}
-		if (alamat_toko == ""){
-			alert("Alamat Harus Diisi");
-		}
-		else { 
-					$.post("proses_edit_toko.php",{id:id,nama_toko:nama_toko,alamat_toko:alamat_toko},function(data){
-
-			if (data != '') {
-			$(".alert").show('fast');
-			$("#table_baru").load('tabel-toko.php');
-			
-			setTimeout(tutupalert, 2000);
-			$(".modal").modal("hide");
-			}
-		
-		
-		});
-		}
-									
-
-		function tutupmodal() {
-		
-		}	
-		});
-		
-
-
-//end function edit data
-
-		$('form').submit(function(){
-		
-		return false;
-		});
-		
-		});
-		
-		
-		
-
-		function tutupalert() {
-		$(".alert").hide("fast")
-		}
-		
-
-
-</script>
-
-<script type="text/javascript">
-	
-  $(function () {
-  $(".table").dataTable({ordering :false });
-  });
-
-</script>
-
-<!-- memasukan file footer.db -->
-<?php include 'footer.php'; ?>
--- toko.php
+++ toko.php
@@ -1,36 +1,38 @@
 <?php include 'session_login.php';
 
-
-//memasukkan file session login, header, navbar, db.php
 include 'header.php';
 include 'navbar.php';
+include 'sanitasi.php';
 include 'db.php';
 
-//menampilkan seluruh data yang ada pada tabel toko
-$query = $db->query("SELECT * FROM toko");
-
- ?>
-
-
-<div class="container"><!--tag yang digunakan untuk membuat tampilan form menjadi rapih dalam satu tempat-->
-
-<h3><b> DATA TOKO</b></h3> <hr>
-
-<?php
-include 'db.php';
-
-$pilih_akses_toko = $db->query("SELECT toko_tambah FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_tambah = '1'");
-$toko = mysqli_num_rows($pilih_akses_toko);
-
-
-    if ($toko > 0){
-// Trigger the modal with a button -->
-echo '<button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal"> <i class="fa fa-plus"> </i> TOKO </button>';
-}
 ?>
 
+
+
+<div class="container">
+
+<h3><b>DATA TOKO</b></h3> <hr>
+
+<?php 
+include 'db.php';
+
+$pilih_akses_toko_tambah = $db->query("SELECT toko_tambah FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_tambah = '1'");
+$toko_tambah = mysqli_num_rows($pilih_akses_toko_tambah);
+
+if ($toko_tambah > 0){
+// Trigger the modal with a button -->
+echo '<button type="button" class="btn btn-info " data-toggle="modal" data-target="#myModal"><i class="fa fa-plus"> </i> TOKO</button>';
+
+}
+
+?>
 <br>
 <br>
+
+
+
+<div class="container">
+<!-- Modal tambah data -->
 
 
 
@@ -47,14 +49,14 @@ echo '<button type="button" class="btn btn-info" data-toggle="modal" data-target
     <div class="modal-body">
 <form role="form">
 					<div class="form-group">
-					<label> Nama toko </label><br>
+					<label> Nama Toko </label><br>
 					<input type="text" name="nama_toko" id="nama_toko" class="form-control" autocomplete="off" required="" >
 					</div>
 					
 					
 					<div class="form-group">
-					<label> alamat_toko </label><br>
-					<textarea type="text" name="alamat_toko" id="alamat_toko" class="form-control" required=""></textarea>
+					<label> Alamat Toko </label><br>
+					<input type="text" name="alamat_toko" id="alamat_toko" class="form-control" autocomplete="off" required="" > 
 					</div>
 					
 					
@@ -84,11 +86,11 @@ echo '<button type="button" class="btn btn-info" data-toggle="modal" data-target
 
 
 
-<!-- Modal content-->
+    <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Konfirmsi Hapus Data Toko</h4>
+        <h4 class="modal-title">Konfirmasi Hapus toko</h4>
       </div>
 
       <div class="modal-body">
@@ -96,7 +98,7 @@ echo '<button type="button" class="btn btn-info" data-toggle="modal" data-target
    <p>Apakah Anda yakin Ingin Menghapus Data ini ?</p>
    <form >
     <div class="form-group">
-    <label> Nama Toko :</label>
+    <label> Nama toko :</label>
      <input type="text" id="data_toko" class="form-control" readonly=""> 
      <input type="hidden" id="id_hapus" class="form-control" > 
     </div>
@@ -111,15 +113,13 @@ echo '<button type="button" class="btn btn-info" data-toggle="modal" data-target
      </div>
 
       <div class="modal-footer">
-        <button type="button" class="btn btn-info" id="btn_jadi_hapus"> <span class='glyphicon glyphicon-ok-sign'> </span> Ya</button>
-        <button type="button" class="btn btn-warning" data-dismiss="modal"> <span class='glyphicon glyphicon-remove-sign'> </span> Batal</button>
+        <button type="button" class="btn btn-info" id="btn_jadi_hapus"> <span class='glyphicon glyphicon-ok-sign'> </span>Ya</button>
+        <button type="button" class="btn btn-warning" data-dismiss="modal"><span class='glyphicon glyphicon-remove-sign'> </span>Batal</button>
       </div>
     </div>
 
   </div>
 </div><!-- end of modal hapus data  -->
-
-
 
 
 <!-- Modal edit data -->
@@ -130,26 +130,24 @@ echo '<button type="button" class="btn btn-info" data-toggle="modal" data-target
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Edit Data Toko</h4>
+        <h4 class="modal-title">Edit Data toko</h4>
       </div>
       <div class="modal-body">
   <form role="form">
    <div class="form-group">
     <label for="email">Nama Toko:</label>
-     <input type="text" class="form-control" id="toko_edit" autocomplete="off"> 
+     <input type="text" class="form-control" id="nama_edit" autocomplete="off"> 
     
    </div>
    
    <div class="form-group">
     <label for="email">Alamat Toko:</label>
-     <input type="text" class="form-control" id="alamat_edit" autocomplete="off"> 
-     <input type="text" class="form-control" id="id_edit" autocomplete="off"> 
+     <input type="text" class="form-control" id="alamat_edit" autocomplete="off">
+     <input type="hidden" class="form-control" id="id_edit">
     
    </div>
    
-
-   
-   <button type="submit" id="submit_edit" class="btn btn-success">Submit</button>
+   <button type="submit" id="submit_edit" class="btn btn-default">Submit</button>
   </form>
   <div class="alert alert-success" style="display:none">
    <strong>Berhasil!</strong> Data Berhasil Di Edit
@@ -165,197 +163,163 @@ echo '<button type="button" class="btn btn-info" data-toggle="modal" data-target
   </div>
 </div><!-- end of modal edit data  -->
 
-<style>
-table {
-    border-collapse: collapse;
-    width: 100%;
-}
-
-th, td {
-    text-align: left;
-    padding: 8px;
-}
-
-tr:nth-child(even){background-color: #f2f2f2}
-
-th {
-    background-color: #4CAF50;
-    color: white;
-}
-</style>
-
-<div class="table-responsive"><!-- membuat agar ada garis pada tabel, disetiap kolom -->
-<span id="table_baru">
-<table id="tableuser" class="table table-bordered">
-		<thead> 
-			
-			<th> Nama toko </th>
-			<th> Alamat Toko</th>
-<?php  
-include 'db.php';
-
-$pilih_akses_toko_hapus = $db->query("SELECT toko_hapus FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_hapus = '1'");
-$toko_hapus = mysqli_num_rows($pilih_akses_toko_hapus);
-
-
-    if ($toko_hapus > 0){
-			echo "<th> Hapus </th>";
-		}
-?>
-
-<?php 
-include 'db.php';
-
-$pilih_akses_toko_edit = $db->query("SELECT toko_edit FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_edit = '1'");
-$toko_edit = mysqli_num_rows($pilih_akses_toko_edit);
-
-
-    if ($toko_edit > 0){
-    	echo "<th> Edit </th>";
-    }
- ?>
-			
-			
-		</thead>
-		
-		<tbody>
-		<?php
-
-		// menyimpan data sementara yang ada pada $query
-			while ($data = mysqli_fetch_array($query))
-			{
-				//menampilkan data
-			echo "<tr>
-			
-			<td>". $data['nama_toko'] ."</td>
-			<td>". $data['alamat_toko'] ."</td>";
-
-include 'db.php';
-
-$pilih_akses_toko_hapus = $db->query("SELECT toko_hapus FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_hapus = '1'");
-$toko_hapus = mysqli_num_rows($pilih_akses_toko_hapus);
-
-
-    if ($toko_hapus > 0){
-
-			echo "<td> <button class='btn btn-danger btn-hapus' data-id='". $data['id'] ."' data-toko='". $data['nama_toko'] ."'> <span class='glyphicon glyphicon-trash'> </span> Hapus </button> </td>";
-		}
-
-include 'db.php';
-
-$pilih_akses_toko_edit = $db->query("SELECT toko_edit FROM otoritas_master_data WHERE id_otoritas = '$_SESSION[otoritas_id]' AND toko_edit = '1'");
-$toko_edit = mysqli_num_rows($pilih_akses_toko_edit);
-
-
-    if ($toko_edit > 0){ 
-			echo "<td> <button class='btn btn-info btn-edit'  data-alamat='". $data['alamat_toko'] ."' data-id='". $data['id'] ."' data-toko='". $data['nama_toko'] ."' > <span class='glyphicon glyphicon-edit'> </span> Edit </button> </td>
-			</tr>";
-			}
-	}
-
-	//Untuk Memutuskan Koneksi Ke Database
-mysqli_close($db);   
-		?>
-		</tbody>
-
-	</table>
+<div class="table-responsive">
+<span id="table-baru">
+<table id="tabel_toko" class="table table-bordered table-sm">
+    <thead>
+      <th> Nama Toko </th> 
+      <th> Alamat Toko </th> 
+      <th> Hapus </th>
+      <th> Edit </th>   
+    </thead>
+  </table>
 </span>
 </div>
-</div> <!-- tag penutup cantainer -->
+</div>
 
-
-							
-<script>
+ <script type="text/javascript">
+  // ajax table toko
     $(document).ready(function(){
 
+        $("#tabel_toko").DataTable().destroy();
+          var dataTable = $('#tabel_toko').DataTable( {
+          "processing": true,
+          "serverSide": true,
+          "ajax":{
+            url :"tabel-toko.php", // json datasource
+            "data": function ( d ) {
+                  d.status = status;
+                  // d.custom = $('#myInput').val();
+                  // etc
+              },
+            type: "post",  // method  , by default get
+            error: function(){  // error handling
+              $(".employee-grid-error").html("");
+              $("# ").append('<tbody class="employee-grid-error"><tr><th colspan="3">Data Tidak Ditemukan.. !!</th></tr></tbody>');
+              $("#employee-grid_processing").css("display","none");
+              
+            }
+          },
+              "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+              $(nRow).attr('class','tr-id-'+aData[4]+'');
+            },
 
+      }); 
+  });
+</script>
+
+<script>
+    $(document).ready(function(){
 //fungsi untuk menambahkan data
-		$("#submit_tambah").click(function(){
-		var nama_toko = $("#nama_toko").val();
-		var alamat_toko = $("#alamat_toko").val();
+    $("#submit_tambah").click(function(){
+    var nama_toko = $("#nama_toko").val();
+    var alamat_toko = $("#alamat_toko").val();
 
-		$("#nama_toko").val('');
-		$("#alamat_toko").val('');
-
-									if (nama_toko == ""){
-									alert("Nama Harus Diisi");
-									}
-									else {
-		
-		$.post('proses_tambah_toko.php',{nama_toko:nama_toko,alamat_toko:alamat_toko},function(data){
-
-		if (data != '') {
-		$("#nama_toko").val('');
-		$("#alamat_toko").val('');
-
-		$(".alert").show('fast');
-		$("#table_baru").load('tabel-toko.php');
-		
-		setTimeout(tutupalert, 2000);
-		$(".modal").modal("hide");
+		if (nama_toko == ""){
+			alert("Nama Harus Diisi");
 		}
-		
-		
-		});										
-									}
+		if (alamat_toko == ""){
+			alert("Alamat Harus Diisi");
+		}
+  
 
-		function tutupmodal() {
-		
-		}		
-		
-		});
+    else{
+
+    $.post('proses_tambah_toko.php',{nama_toko:nama_toko,alamat_toko:alamat_toko},function(data){
+
+    if (data != '') {
+    $("#nama_toko").val('');
+    $(".alert").show('fast');
+    
+    setTimeout(tutupalert, 2000);
+    $(".modal").modal("hide");
+    }
+
+        $("#tabel_toko").DataTable().destroy();
+          var dataTable = $('#tabel_toko').DataTable( {
+          "processing": true,
+          "serverSide": true,
+          "ajax":{
+            url :"tabel-toko.php", // json datasource
+            "data": function ( d ) {
+                  d.status = status;
+                  // d.custom = $('#myInput').val();
+                  // etc
+              },
+            type: "post",  // method  , by default get
+            error: function(){  // error handling
+              $(".employee-grid-error").html("");
+              $("#tabel_toko").append('<tbody class="employee-grid-error"><tr><th colspan="4">Data Tidak Ditemukan.. !!</th></tr></tbody>');
+              $("#employee-grid_processing").css("display","none");
+              
+            }
+          },
+              "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+              $(nRow).attr('class','tr-id-'+aData[4]+'');
+            },
+
+      }); 
+    
+    
+    });
+    }
+    
+    
+    function tutupmodal() {
+    
+    }
+    });
 
 // end fungsi tambah data
 
 
-	
+  
 //fungsi hapus data 
-		$(".btn-hapus").click(function(){
-		var nama = $(this).attr("data-toko");
-		var id = $(this).attr("data-id");
-		$("#data_toko").val(nama);
-		$("#id_hapus").val(id);
-		$("#modal_hapus").modal('show');
-		
-		
-		});
+$(document).on('click', '.btn-hapus', function (e) {
+    var nama_toko = $(this).attr("data-toko");
+    var id = $(this).attr("data-id");
+    $("#data_toko").val(nama_toko);
+    $("#id_hapus").val(id);
+    $("#modal_hapus").modal('show');
+    
+    
+    });
 
 
-		$("#btn_jadi_hapus").click(function(){
-		
-		var id = $("#id_hapus").val();
+$(document).on('click', '#btn_jadi_hapus', function (e) {
+    
+    var id = $("#id_hapus").val();
+    $.post("proses_hapus_toko.php",{id:id},function(data){
+    if (data != "") {
+    
+    $("#modal_hapus").modal('hide');
+    $(".tr-id-"+id+"").remove();
+    
+    }
 
-		$.post("proses_hapus_toko.php",{id:id},function(data){
-
-		if (data != "") {
-		$("#table_baru").load('tabel-toko.php');
-		$("#modal_hapus").modal('hide');
-		
-		}
-
-		
-		});
-		
-		});
+    
+    });
+    
+    });
 // end fungsi hapus data
 
 //fungsi edit data 
-		$(".btn-edit").click(function(){
-		
-		$("#modal_edit").modal('show');
-		var nama_toko = $(this).attr("data-toko"); 
-		var alamat_toko = $(this).attr("data-alamat");
-		var id  = $(this).attr("data-id");
-		$("#toko_edit").val(nama_toko);
-		$("#alamat_edit").val(alamat_toko);
-		$("#id_edit").val(id);
-		 
-		});
-		
-		$("#submit_edit").click(function(){
-		var nama_toko = $("#toko_edit").val();
-		var alamat_toko = $("#alamat_edit").val();
-		var id = $("#id_edit").val();
+    $(document).on('click', '.btn-edit', function (e) {
+    
+    $("#modal_edit").modal('show');
+    var nama_toko = $(this).attr("data-toko"); 
+	var alamat_toko = $(this).attr("data-alamat");
+    var id  = $(this).attr("data-id");
+    $("#nama_edit").val(nama_toko);
+	$("#alamat_edit").val(alamat_toko);
+    $("#id_edit").val(id); 
+    
+    });
+    
+    $("#submit_edit").click(function(){
+    var nama_toko = $("#nama_edit").val();
+    var alamat_toko = $("#alamat_edit").val();
+    var id = $("#id_edit").val();
 
 		if (nama_toko == ""){
 			alert("Nama Harus Diisi");
@@ -373,46 +337,58 @@ mysqli_close($db);
 			setTimeout(tutupalert, 2000);
 			$(".modal").modal("hide");
 			}
-		
-		
-		});
-		}
-									
+		     $("#tabel_toko").DataTable().destroy();
+          var dataTable = $('#tabel_toko').DataTable( {
+          "processing": true,
+          "serverSide": true,
+          "ajax":{
+            url :"tabel-toko.php", // json datasource
+            "data": function ( d ) {
+                  d.status = status;
+                  // d.custom = $('#myInput').val();
+                  // etc
+              },
+            type: "post",  // method  , by default get
+            error: function(){  // error handling
+              $(".employee-grid-error").html("");
+              $("#tabel_toko").append('<tbody class="employee-grid-error"><tr><th colspan="4">Data Tidak Ditemukan.. !!</th></tr></tbody>');
+              $("#employee-grid_processing").css("display","none");
+              
+            }
+          },
+              "fnCreatedRow": function( nRow, aData, iDataIndex ) {
+              $(nRow).attr('class','tr-id-'+aData[4]+'');
+            },
 
-		function tutupmodal() {
+      }); 
 		
-		}	
 		});
-		
+		} 
+    });
+    
 
 
 //end function edit data
 
-		$('form').submit(function(){
-		
-		return false;
-		});
-		
-		});
-		
-		
-		
+    $('form').submit(function(){
+    
+    return false;
+    });
+    
+    });
+    
+    
+    
+    
+    function tutupalert() {
+    $(".alert").hide("fast");
 
-		function tutupalert() {
-		$(".alert").hide("fast")
-		}
-		
+    }
+    
 
-
-</script>
-
-<script type="text/javascript">
-	
-  $(function () {
-  $(".table").dataTable({ordering :false });
-  });
 
 </script>
 
-<!-- memasukan file footer.db -->
+
 <?php include 'footer.php'; ?>
+
