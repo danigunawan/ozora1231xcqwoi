2f510b0c472378a3298f17cc8a3d0bd3fe44b57f
RindangRamadhan <rindangramadhan10@gmail.com>
2017-08-24 13:44:13 +0700

    Aduan Implemntator
    Merubah penamaan level harga, Level 1 = Marketplace dan Level 2 = admin

--- form_order_penjualan.php
+++ form_order_penjualan.php
@@ -159,9 +159,8 @@ $session_id = session_id();
 <div class="col-sm-2">
     <label> Level Harga </label><br>
     <select style="font-size:15px; height:35px" type="text" name="level_harga" id="level_harga" class="form-control chosen" required="" >
-      <option value="Level 1">Level 1</option>
-      <option value="Level 2">Level 2</option>
-      <option value="Level 3">Level 3</option>
+      <option value="Level 1">Marketplace</option>
+      <option value="Level 2">Admin</option>
     </select>
     </div>
 
